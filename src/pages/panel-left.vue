75a341193375e0d2c886958e7fe1f029cc8ce1f0
César Garcia <cesar@descom.es>
2018-04-20 18:41:14 +0200

    Cambio colores

--- src/pages/panel-left.vue
+++ src/pages/panel-left.vue
@@ -12,6 +12,19 @@
     </f7-list>
   </f7-page>
 </template>
+<style>
+.md .theme-dark .page {
+background: rgba(79,101,62,1);
+background: -moz-linear-gradient(left, rgba(79,101,62,1) 0%, rgba(24,45,6,0.95) 100%);
+background: -webkit-gradient(left top, right top, color-stop(0%, rgba(79,101,62,1)), color-stop(100%, rgba(24,45,6,0.95)));
+background: -webkit-linear-gradient(left, rgba(79,101,62,1) 0%, rgba(24,45,6,0.95) 100%);
+background: -o-linear-gradient(left, rgba(79,101,62,1) 0%, rgba(24,45,6,0.95) 100%);
+background: -ms-linear-gradient(left, rgba(79,101,62,1) 0%, rgba(24,45,6,0.95) 100%);
+background: linear-gradient(to right, rgba(79,101,62,1) 0%, rgba(24,45,6,0.95) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#4f653e', endColorstr='#182d06', GradientType=1 );
+}
+
+</style>
 <script>
 export default {}
 </script>
